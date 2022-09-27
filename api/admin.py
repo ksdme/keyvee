53765f38389d42caf84dac78f508b5606a407fd5
Kilari Teja <kilariteja9@gmail.com>
2022-09-27 11:56:39 +0530

    Add Blob model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import Blob
 
-# Register your models here.
+
+@admin.register(Blob)
+class BlobModelAdmin(admin.ModelAdmin):
+  list_display = ("owner", "key", "created", "updated")
--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Blob(models.Model):
+    """
+    Represents a blob on the keyvee platform.
+    """
+    owner = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    key = models.CharField(max_length=128, db_index=True)
+    value = models.JSONField(null=True, default=None, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("owner", "key")
